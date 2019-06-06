f205c332eca7b3e03667a0291167c576a41f0eee
ramathodupunuri <50694090+ramathodupunuri@users.noreply.github.com>
2019-06-06 06:16:32 -0400

    changes 06-06-2019

---
+++ src/main/java/com/roytuts/springmvc/mapper/RequirementRowMapper.java
@@ -0,0 +1,24 @@
+package com.roytuts.springmvc.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.roytuts.springmvc.model.Requirement;
+
+public class RequirementRowMapper implements RowMapper<Requirement> {
+
+	@Override
+	public Requirement mapRow(ResultSet rs, int row) throws SQLException {
+		Requirement requirement = new Requirement();
+		requirement.setJobcode(rs.getString("jobcode"));
+		requirement.setExperiencelevel(rs.getString("experiencelevel"));
+		requirement.setSkillset(rs.getString("skillset"));
+		requirement.setTechnology(rs.getString("technology"));
+		requirement.setUploadedby(rs.getString("uploadedby"));
+		requirement.setDepartment(rs.getString("department"));
+		return requirement;
+	}
+
+}
